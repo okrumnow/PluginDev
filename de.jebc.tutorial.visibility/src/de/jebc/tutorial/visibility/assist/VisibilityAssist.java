7d2625c3053dc2c14e76decfda066152d1190aab
Olaf Krumnow <okrumnow@gmx.de>
2011-08-20 08:53:33 +0200

    find method declaration

--- de.jebc.tutorial.visibility/src/de/jebc/tutorial/visibility/assist/VisibilityAssist.java
+++ de.jebc.tutorial.visibility/src/de/jebc/tutorial/visibility/assist/VisibilityAssist.java
@@ -1,6 +1,8 @@
 package de.jebc.tutorial.visibility.assist;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.ui.text.java.IInvocationContext;
 import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
 import org.eclipse.jdt.ui.text.java.IProblemLocation;
@@ -16,7 +18,19 @@ public class VisibilityAssist implements IQuickAssistProcessor {
 	@Override
 	public IJavaCompletionProposal[] getAssists(IInvocationContext context,
 			IProblemLocation[] locations) throws CoreException {
-		return null;
+		ASTNode coveringNode = context.getCoveringNode();
+		MethodDeclaration declaration = findDeclaration(coveringNode);
+		if (declaration == null) return null;
+		return new IJavaCompletionProposal[] {};
+	}
+
+	private MethodDeclaration findDeclaration(ASTNode coveringNode) {
+		MethodDeclaration result = null;
+		if (coveringNode instanceof MethodDeclaration)
+			result = (MethodDeclaration) coveringNode;
+		else if (coveringNode.getParent() instanceof MethodDeclaration)
+			result = (MethodDeclaration) coveringNode.getParent();
+		return result;
 	}
 
 }
